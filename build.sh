b7377031463703dce72d39f49ea3177afc6df066
Kevin Giovinazzo <g3kappa@gmail.com>
2021-06-21 19:42:19 +0200

    better build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-#! /bin/sh
-
-ls -ld $(find .) | grep -oh \./.*/_.*\.txt | xargs cat > furniture.txt
+#! /bin/bash
+if test $# -ne 1; then 
+    echo 'Usage: build.sh <mod folder>'
+else
+    ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs cat > furniture.txt
+    ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs cat >> furniture.txt
+fi
